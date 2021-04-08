6a6603e9f2a34c4f2a25c1c8362ab9df7188a7ce
Eva <camsboardprofile15@gmail.com>
2021-04-08 00:23:20 -0400

    First init

---
+++ main.py
@@ -0,0 +1,40 @@
+import requests
+from bs4 import BeautifulSoup
+import wikipedia
+import time
+import os
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from string import Template
+
+from dotenv import load_dotenv
+load_dotenv()
+
+randomLink = 'https://en.wikipedia.org/wiki/Special:Random'
+
+request = requests.get(randomLink)
+content = BeautifulSoup(request.content, 'html.parser')
+
+title = content.find(id = 'firstHeading')
+summary = wikipedia.summary(title)
+
+url = 'https://en.wikipedia.org/wiki/%s ' %title
+
+server = smtplib.SMTP( host = 'smtp.gmail.com', port = 587)
+server.starttls()
+server.login('dailywikipediaarticle@gmail.com', os.environ.get('Password'))
+
+
+messageTemplateFile = open('messageTemplate.txt', 'r')
+messageTemplate = messageTemplateFile.read()
+
+message = MIMEMultipart()
+
+message['From'] = 'DailyWikipedia dailywikipediaarticle@gmail.com'
+message['To'] = 'camsboardprofile15@gmail.com'
+message['Subject'] = 'Test subject'
+
+message.attach(MIMEText(messageTemplate, 'plain'))
+
+server.send_message(message)
