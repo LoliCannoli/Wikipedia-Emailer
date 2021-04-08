b471e9ae2b5b531ea46c8270e2d95a2905ef2b78
Eva <camsboardprofile15@gmail.com>
2021-04-08 13:46:45 -0400

    added emailing functionality

--- main.py
+++ main.py
@@ -1,39 +1,36 @@
-import requests
+from sendEmail import sendEmail
 from bs4 import BeautifulSoup
-import wikipedia
-import os
-import smtplib
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-from string import Template
-
 from dotenv import load_dotenv
+import wikipedia
+import requests
+import smtplib
+import os
+
 load_dotenv()
 
-randomLink = 'https://en.wikipedia.org/wiki/Special:Random'
+RANDOMLINK = 'https://en.wikipedia.org/wiki/Special:Random'
+EMAIL = 'dailywikipediaarticle@gmail.com'
+HOST = 'smtp.gmail.com'
+PORT = 587
 
-request = requests.get(randomLink)
+template = 'messageTemplate.txt'
+
+server = smtplib.SMTP( host = HOST, port = PORT)
+server.starttls()
+server.login(EMAIL, os.environ.get('Password'))
+
+request = requests.get(RANDOMLINK)
 content = BeautifulSoup(request.content, 'html.parser')
 
-title = content.find(id = 'firstHeading').text
-summary = wikipedia.summary(title)
+title, summary = None, None
+while title is None and summary is None:
+    try:
+        title = content.find(id = 'firstHeading').text
+        summary = wikipedia.summary(title)
+    except:
+        pass
 
-url = 'https://en.wikipedia.org/wiki/%s ' %title
 
-server = smtplib.SMTP( host = 'smtp.gmail.com', port = 587)
-server.starttls()
-server.login('dailywikipediaarticle@gmail.com', os.environ.get('Password'))
+url = 'https://en.wikipedia.org/wiki/%s' %title.replace(' ', '_')
 
-
-messageTemplateFile = open('messageTemplate.txt', 'r')
-messageTemplate = messageTemplateFile.read()
-
-message = MIMEMultipart()
-
-message['From'] = 'DailyWikipedia dailywikipediaarticle@gmail.com'
-message['To'] = 'camsboardprofile15@gmail.com'
-message['Subject'] = 'Your daily topic is: %s ' %title
-
-message.attach(MIMEText(messageTemplate, 'plain'))
-
-server.send_message(message)
+sendEmail(title, summary, url, template, server)
